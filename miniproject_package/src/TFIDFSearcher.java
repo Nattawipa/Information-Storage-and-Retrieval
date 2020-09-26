53a0b00aa82831e4b84ea4a1b6e694d5ccea4554
China <kittikorn.kee@student.mahidol.edu>
2020-09-27 01:56:27 +0700

    update TFIDF

--- miniproject_package/src/TFIDFSearcher.java
+++ miniproject_package/src/TFIDFSearcher.java
@@ -4,23 +4,46 @@
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class TFIDFSearcher extends Searcher {
+	private int docSize = this.documents.size();
+	private HashSet<String> setIdf;
+	private HashMap<String, Double> allIdf = new HashMap<String, Double>();
+
 	public TFIDFSearcher(String docFilename) {
 		/************* YOUR CODE HERE ******************/
 		super(docFilename);
+		for (int i = 0; i < docSize; i++) {
+			setIdf = new HashSet<>(this.documents.get(i).getTokens());
+			for (String a : setIdf) {
+				if (!allIdf.containsKey(a))
+					allIdf.put(a, 0.0);
+				allIdf.replace(a, allIdf.get(a) + 1);
+			}
+		}
+
+		for (String a : allIdf.keySet()) {
+			allIdf.replace(a, Math.log10(1 + (docSize / allIdf.get(a))));
+		}
 		/***********************************************/
 	}
 
 	@Override
 	public List<SearchResult> search(String queryString, int k) {
 		/************* YOUR CODE HERE ******************/
+
 		List<String> queryWords = super.tokenize(queryString);
 		List<SearchResult> result = new ArrayList<SearchResult>();
-		HashMap<String, Double> termFreqMap = new HashMap<String, Double>();
-
-		for (int document = 0; document < 1; document++) {
+		HashMap<String, Double> termFreqTF = new HashMap<String, Double>();
+		if (queryString.isEmpty()) {
+			for (int i = 0; i < k; i++) {
+				result.add(new SearchResult(documents.get(i), Double.NaN));
+			}
+			return result;
+		}
+		for (int document = 0; document < docSize; document++) {
 			List<String> wordsEachDocument = this.documents.get(document).getTokens();
 			for (String token : queryWords) {
 				double count = 0;
@@ -29,10 +52,15 @@ public class TFIDFSearcher extends Searcher {
 						count++;
 					}
 				}
-				termFreqMap.put(token, count);
+				if (count != 0) {
+					termFreqTF.put(token, 1 + Math.log10(count));
+				} else {
+					termFreqTF.put(token, 0.0);
+				}
+
 			}
 		}
-		System.out.println(termFreqMap);
+		// System.out.println(termFreqMap);
 
 		return null;
 		/***********************************************/
